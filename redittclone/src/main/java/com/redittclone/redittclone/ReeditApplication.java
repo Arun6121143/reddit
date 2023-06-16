ad7503b62f0be54ee6f230fdba304b9f444b2d22
ArunKumar  Menon <110023519+Arun6121143@users.noreply.github.com>
2023-06-16 22:31:11 +0530

    Add files via upload

---
+++ redittclone/src/main/java/com/redittclone/redittclone/ReeditApplication.java
@@ -0,0 +1,11 @@
+package com.redittclone.redittclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReeditApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ReeditApplication.class, args);
+	}
+}
---
+++ redittclone/src/test/java/com/redittclone/redittclone/RedittcloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redittclone.redittclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedittcloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
